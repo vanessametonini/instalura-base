cfe6bc5d0af483f3a2285ffe56e9664eb3d41c49
Vanessa Me Tonini <vanessametonini@gmail.com>
2021-02-14 15:46:14 -0300

    Aula02

---
+++ src/components/commons/Menu/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Logo } from "../../theme/Logo";
+import { MenuWrapper } from "./styles/MenuWrapper";
+
+
+const links  = [
+  {
+    texto: 'Home',
+    url: '/home'
+  },
+  {
+    texto: 'Perguntas frequentes',
+    url: '/faq'
+  },
+  {
+    texto: 'Sobre',
+    url: '/sobre'
+  },
+]
+export function Menu() {
+  return (
+    <MenuWrapper>
+      <MenuWrapper.LeftSide>
+        <Logo />
+      </MenuWrapper.LeftSide>
+      <MenuWrapper.CentralSide>
+        {links.map((link) => {
+          return <li key={link.url}>
+            <a href={link.url}>
+              {link.texto}
+            </a>
+          </li>
+        })}
+      </MenuWrapper.CentralSide>
+      <MenuWrapper.RightSide>
+        <button>Entrar</button>
+        <button>Cadastrar</button>
+      </MenuWrapper.RightSide> 
+    </MenuWrapper>
+  )
+}
+
